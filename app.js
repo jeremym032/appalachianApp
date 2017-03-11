9e5812e5cbb0b69cc30dfc750cd53d916a450d8c
Jeremy Martin <Jeremy Martin>
2017-03-11 12:00:59 -0500

    Adding index.ejs

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+	res.render('index');
+}
+
+app.listen(3000, function(){
+	console.log('Test App is running');
+}
