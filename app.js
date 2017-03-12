00413b313cc8fdeb2541cf5d03df85bc0e8036b6
root <root@jcm-web-server.localdomain>
2017-03-12 19:23:17 +0000

    Adding bootstrap files

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
 	res.render('index');
-}
+});
 
-app.listen(3000, function(){
+app.listen(5000, function(){
 	console.log('Test App is running');
-}
+});
